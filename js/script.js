3b87a477ae9b3a5b3dc60f1679f30acc629f2ec5
Sakshi-369-Pro <projects.mail369@gmail.com>
2026-03-18 19:25:40 +0530

    updated portfolio

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-// reveal animation
+const words = ["Data Analyst_", "AI Engineer_", "Programmer_", "Web Developer_"];
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+  current = words[i];
+  document.querySelector(".typing").textContent = current.substring(0, j);
+
+  if (!isDeleting && j++ === current.length) {
+    isDeleting = true;
+  } 
+  else if (isDeleting && j-- === 0) {
+    isDeleting = false;
+    i = (i + 1) % words.length;
+  }
+
+  setTimeout(type, isDeleting ? 60 : 100);
+}
+
+
+document.addEventListener("DOMContentLoaded", type);
+
+
 const reveals = document.querySelectorAll(".reveal");
 
 window.addEventListener("scroll", () => {
@@ -13,7 +37,6 @@ window.addEventListener("scroll", () => {
 });
 
 
-// PROJECT CARD CLICK TOGGLE (ADDED)
 const projectCards = document.querySelectorAll(".project-card");
 
 projectCards.forEach(card => {
